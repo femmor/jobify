247e53a618ff17655b4c7ac9bfbe52dd2d3b4974
Emmanuel Egomson <fegomson@gmail.com>
2022-05-24 00:56:03 -0500

    FormRow and alert component implementation

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,4 +1,17 @@
+import Wrapper from '../assets/wrappers/ErrorPage';
+import errorImage from '../assets/images/not-found.svg';
+import { Link } from 'react-router-dom';
+
 const Error = () => {
-  return <h1>Error</h1>;
+  return (
+    <Wrapper className="full-page">
+      <div>
+        <img src={errorImage} alt="not found" />
+        <h3>Oops! page not found</h3>
+        <p>We can't seem to find the page you are looking for.</p>
+        <Link to="/">back home</Link>
+      </div>
+    </Wrapper>
+  );
 };
 export default Error;
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,4 +1,66 @@
+import { useState, useEffect } from 'react';
+import { Alert, FormRow, Logo } from '../components';
+import Wrapper from '../assets/wrappers/RegisterPage';
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+  showAlert: false,
+};
+
 const Register = () => {
-  return <h1>Register</h1>;
+  const [values, setValues] = useState(initialState);
+
+  // global state and useNavigate hook
+
+  const handleChange = e => {
+    console.log(e.target);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log('submit');
+  };
+
+  // Destructure values
+  const { name, email, password, isMember, showAlert } = values;
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={handleSubmit}>
+        <Logo />
+        <h3>{isMember ? 'Login' : 'Register'}</h3>
+        {showAlert && <Alert type="success" message="Alert message" />}
+        {!isMember && (
+          <FormRow
+            name={name}
+            type="text"
+            value={name}
+            handleChange={handleChange}
+            labelText="Name"
+          />
+        )}
+        <FormRow
+          name={email}
+          type="email"
+          value={email}
+          handleChange={handleChange}
+          labelText="Email"
+        />
+        <FormRow
+          name={password}
+          type="password"
+          value={password}
+          handleChange={handleChange}
+          labelText="Password"
+        />
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+      </form>
+    </Wrapper>
+  );
 };
 export default Register;
