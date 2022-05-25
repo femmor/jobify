ef303aea1237282887862ed04a08cfd15406cb9f
Emmanuel Egomson <fegomson@gmail.com>
2022-05-24 22:23:35 -0500

    Not Found middleware functionality

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import express from 'express';
+import notFoundMiddleware from './middleware/not-found.js';
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('server is working!');
 });
+
+// Middleware
+app.use(notFoundMiddleware);
 
 const PORT = process.env.PORT || 3005;
 
