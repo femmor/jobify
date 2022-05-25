2c2ebb693e55edb88be7aefc284c950d35ca7e08
Emmanuel Egomson <fegomson@gmail.com>
2022-05-25 12:13:50 -0500

    Remove stimestamp

--- models/User.js
+++ models/User.js
@@ -1,41 +1,36 @@
 import mongoose from 'mongoose';
 
-const UserSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, 'Please provide a name'],
-      minLength: 3,
-      maxLength: 20,
-      trim: true,
-    },
-    email: {
-      type: String,
-      required: [true, 'Please provide an email'],
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: [true, 'Please provide a password'],
-      minLength: 6,
-    },
-    lastName: {
-      type: String,
-      maxLength: 20,
-      trim: true,
-      default: 'Last Name',
-    },
-    location: {
-      type: String,
-      maxLength: 20,
-      trim: true,
-      default: 'My City',
-    },
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide a name'],
+    minLength: 3,
+    maxLength: 20,
+    trim: true,
   },
-  {
-    timestamps: true,
-  }
-);
+  email: {
+    type: String,
+    required: [true, 'Please provide an email'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minLength: 6,
+  },
+  lastName: {
+    type: String,
+    maxLength: 20,
+    trim: true,
+    default: 'Last Name',
+  },
+  location: {
+    type: String,
+    maxLength: 20,
+    trim: true,
+    default: 'My City',
+  },
+});
 
 const user = mongoose.model('User', UserSchema);
 
