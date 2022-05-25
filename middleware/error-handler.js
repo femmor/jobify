01fa4879eae7ca36b5fa898479ef1b91f5c8059e
Emmanuel Egomson <fegomson@gmail.com>
2022-05-25 14:52:10 -0500

    Handling missing field errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -3,11 +3,17 @@ import { StatusCodes } from 'http-status-codes';
 const errorHandlerMiddleware = (err, req, res, next) => {
   console.error(err);
   const defaultError = {
-    message: 'Something went wrong, please try again later!',
     statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+    message: 'Something went wrong, please try again later!',
   };
+  if (err.name === 'ValidationError') {
+    defaultError.statusCode = StatusCodes.BAD_REQUEST;
+    defaultError.message = Object.values(err.errors)
+      .map(item => item.message)
+      .join(', ');
+  }
   res.status(defaultError.statusCode).json({
-    message: err.message || defaultError.message,
+    message: defaultError.message,
   });
 };
 
