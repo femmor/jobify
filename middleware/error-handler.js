b3ac38c82bcf877f81bb86417d1e29d7f57f86ae
Emmanuel Egomson <fegomson@gmail.com>
2022-05-25 16:01:03 -0500

    Check for empty values in controller

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,9 @@
 import { StatusCodes } from 'http-status-codes';
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-  console.error(err);
   const defaultError = {
     statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
-    message: 'Something went wrong, please try again later!',
+    message: err.message || 'Something went wrong, please try again later!',
   };
   if (err.name === 'ValidationError') {
     defaultError.statusCode = StatusCodes.BAD_REQUEST;
