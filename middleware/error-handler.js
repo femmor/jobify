df45529b77c29fa6fa685541a4fad64fbeacbfee
Emmanuel Egomson <fegomson@gmail.com>
2022-05-25 12:51:37 -0500

    Set up defaultError

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,7 +1,13 @@
+import { StatusCodes } from 'http-status-codes';
+
 const errorHandlerMiddleware = (err, req, res, next) => {
   console.error(err);
-  res.status(500).json({
-    message: err,
+  const defaultError = {
+    message: 'Something went wrong, please try again later!',
+    statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+  };
+  res.status(defaultError.statusCode).json({
+    message: err.message || defaultError.message,
   });
 };
 
